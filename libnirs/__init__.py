9c64e6aadf431399c8dbd102cf621529c91d993e
Julia Tatz <julia.tatz@childrens.harvard.edu>
2020-03-12 02:41:35 -0400

    Generalized n-layer model & improved numerical accuracy + speed

--- libnirs/__init__.py
+++ libnirs/__init__.py
@@ -2,7 +2,7 @@ import libnirs.overrides
 from .utils import integrate
 from .extinction_coeffs import get_extinction_coeffs
 from .model import model_ss, model_fd, model_td, model_g2
-from .layered_model import gen_mathmatica_code, model_nlayer_ss, model_nlayer_fd, model_nlayer_g2
+from .layered_model import model_nlayer_ss, model_nlayer_fd, model_nlayer_g2
 from .cuda import cuda_model_ss, cuda_model_fd, cuda_model_td, cuda_model_g2, cuda_model_nlayer_ss, cuda_model_nlayer_fd, cuda_model_nlayer_g2
 from .fitting import Fit
 from .qrs import detect_qrs
--- libnirs/layered_model.py
+++ libnirs/layered_model.py
@@ -1,58 +1,87 @@
 from numpy import pi, exp, sqrt, sinh, cosh
 from scipy.special import j0
-# from .overrides import _vector_j0 as j0
 from .utils import jit, integrate, gen_coeffs
 from .model import model_ss, model_fd, model_g2
 
+"""
+WARNING
+The D and alpha values are not precomputed due to the fact that the
+number of layers isn't known during numba compilation, and the dynamic
+memory allocation needed for storing the precumputed values isn't allowed
+for the cuda target.
+"""
 
-def gen_mathmatica_code(layer_count):
-    """Generates Mathmatica code to generate python code for n>1 layer models"""
-    initals = ", ".join('D[p%d[z, s], {z, 2}] - a%d^2 p%d[z, s] == 0' % (i, i, i) for i in range(2, layer_count+1))
-    boundrys = ", ".join('p{0}[l{0}, s] == p{1}[l{0}, s], D{0} (D[p{0}[z, s], z] /. z -> l{0}) == D{1} (D[p{1}[z, s], z] /. z -> l{0})'.format(i, i+1) for i in range(1, layer_count))
-    funcs = ", ".join("p{}[z, s]".format(i+1) for i in range(layer_count))
-    assumptions = " && ".join("a{0} > 0 && D{0} > 0".format(i+1) for i in range(layer_count))
-    layers = " > ".join("l{}".format(i) for i in range(layer_count - 1, 0, -1))
-    code = """
-solvs = DSolve[{D[p1[z, s], {z, 2}] - a1^2 p1[z, s] == -DiracDelta[z - z0]/D1, %s, p1[-zb, s] == 0, p%d[Infinity, s] == 0, %s }, {%s}, {z, s}, Assumptions -> %s && %s > z0 > 0 && zb > 0];
-soln = FullSimplify[First[p1[z, s] /. solvs]]
-pw = PageWidth /. Options[$Output];
-SetOptions[$Output, PageWidth -> Infinity];
-FortranForm[Simplify[soln, z < z0] //. {Exp[x_] :> exp[x], Sinh[x_] :> sinh[x], Cosh[x_] :> cosh[x]}]
-SetOptions[$Output, PageWidth -> pw];""" % (initals, layer_count, boundrys, funcs, assumptions, layers)
-    return code
+@jit
+def _n_layer_refl(s, z, z0, zb, ls, muas, musps, alphas, alpha_args, flu_coeff, refl_coeff):
+    n = len(muas)
+    
+    def ds(i):
+        return 1 / (3 * (muas[i] + musps[i]))
+    
+    d2 = ds(n-1)
+    d1 = ds(n-2)
+    alpha2 = alphas(s, n-1, d2, *alpha_args)
+    alpha1 = alphas(s, n-2, d1, *alpha_args)
+
+    if n == 2:
+        l1 = ls[0]
+        coeff_n = d1 * alpha1 * cosh(alpha1 * (l1 - z0)) + d2 * alpha2 * sinh(alpha1 * (l1 - z0))
+        coeff_d = d1 * (d1 * alpha1 * cosh(alpha1 * (l1 + zb)) + d2 * alpha2 * sinh(alpha1 * (l1 + zb)))
+        coeff = coeff_n / coeff_d
+        phi = coeff * sinh(alpha1 * (z + zb)) / alpha1
+        dz_phi = coeff * cosh(alpha1 * (z + zb))
+        return flu_coeff * phi + refl_coeff * d1 * dz_phi
+    
+    k_num = (alpha1 * d1 - alpha2 * d2) * exp(-ls[-1] * (alpha1 + alpha2))
+    k_dem = (alpha1 * d1 + alpha2 * d2) * exp( ls[-1] * (alpha1 - alpha2))
+
+    d1, d2 = ds(n-3), d1
+    alpha1, alpha2 = alphas(s, n-3, d1, *alpha_args), alpha1
+    for i in range(n - 3, 0, -1):
+        adi = alpha1 * d1
+        adn = alpha2 * d2
+        kaa = (adi + adn) * exp(-ls[i] * (alpha1 - alpha2))
+        kab = (adi - adn) * exp(-ls[i] * (alpha1 + alpha2))
+        kba = (adi - adn) * exp( ls[i] * (alpha1 + alpha2))
+        kbb = (adi + adn) * exp( ls[i] * (alpha1 - alpha2))
+        k_num, k_dem = kaa * k_num + kab * k_dem, kba * k_num + kbb * k_dem
+        
+        d1, d2 = ds(i-1), d1
+        alpha1, alpha2 = alphas(s, i - 1, d1, *alpha_args), alpha1
+    l1 = ls[0]
+    coeff = (
+        exp(zb*alpha1) * (
+            alpha1 * d1 * (exp(2 * l1 *alpha2) * k_num + k_dem) *
+            cosh(alpha1*(l1-z0)) +
+            alpha2 * d2 * (-exp(2 * l1 *alpha2) * k_num + k_dem) *
+            sinh(alpha1*(l1-z0))
+        ) / (
+            2 * alpha1 * d1 * (
+                alpha1 * d1 * (exp(2 * l1 *alpha2) * k_num + k_dem) *
+                cosh(alpha1*(l1+zb)) +
+                alpha2 * d2 * (-exp(2 * l1 *alpha2) * k_num + k_dem) *
+                sinh(alpha1*(l1+zb))
+            )
+        )
+    )
+
+    phi = 2 * coeff * exp(-alpha1 * zb) * sinh(alpha1 * (zb + z))
+    dz_phi = 2 * alpha1 * coeff * exp(-alpha1 * zb) * cosh(alpha1 * (zb + z))
+    return flu_coeff * phi + refl_coeff * d1 * dz_phi
 
 
 @jit
-def _2_layer_phi(s, z, z0, zb, depths, D, alpha_args):
-    l = depths[0]
-    D1, D2 = D[0], D[1]
-    a1, a2 = sqrt(s**2 + alpha_args[0]), sqrt(s**2 + alpha_args[1])
-    return (2*exp(a1*(l + zb))*(a1*D1*cosh(a1*(l - z0)) + a2*D2*sinh(a1*(l - z0)))*sinh(a1*(z + zb)))/(a1*D1*(a2*D2*(-1 + exp(2*a1*(l + zb))) + a1*D1*(1 + exp(2*a1*(l + zb)))))
+def _refl_integrator(s, z, rho, z0, zb, ls, muas, musps, alphas, alpha_args, flu_coeff, refl_coeff):
+    return s*j0(s*rho)*_n_layer_refl(s, z, z0, zb, ls, muas, musps, alphas, alpha_args, flu_coeff, refl_coeff)
 
 
 @jit
-def _3_layer_phi(s, z, z0, zb, depths, D, alpha_args):
-    l1, l2 = depths[0], depths[1]
-    D1, D2, D3 = D[0], D[1], D[2]
-    a1, a2, a3 = sqrt(s**2 + alpha_args[0]), sqrt(s**2 + alpha_args[1]), sqrt(s**2 + alpha_args[2])
-    return ((((a1*D1*cosh(a1*(l1 - z0))*(a2*D2*cosh(a2*(l1 - l2)) - a3*D3*sinh(a2*(l1 - l2))) + a2*D2*(a3*D3*cosh(a2*(l1 - l2)) - a2*D2*sinh(a2*(l1 - l2)))*sinh(a1*(l1 - z0)))*sinh(a1*(z + zb)))/(-(sinh(a2*(l1 - l2))*(a1*a3*D1*D3*cosh(a1*(l1 + zb)) + a2**2*D2**2*sinh(a1*(l1 + zb)))) + a2*D2*cosh(a2*(l1 - l2))*(a1*D1*cosh(a1*(l1 + zb)) + a3*D3*sinh(a1*(l1 + zb)))))/(a1*D1))
+def _ss_alphas(s, i, d, muas):
+    return sqrt(s**2 + muas[i] / d)
 
 
 @jit
-def _4_layer_phi(s, z, z0, zb, depths, D, alpha_args):
-    l1, l2, l3 = depths[0], depths[1], depths[2]
-    D1, D2, D3, D4 = D[0], D[1], D[2], D[3]
-    a1, a2, a3, a4 = sqrt(s**2 + alpha_args[0]), sqrt(s**2 + alpha_args[1]), sqrt(s**2 + alpha_args[2]), sqrt(s**2 + alpha_args[3])
-    return (exp(-(a1*(z + z0)))*(-1 + exp(2*a1*(z + zb)))*(a1*D1*(exp(2*a1*l1) + exp(2*a1*z0))*(a3*D3*sinh(a2*(l1 - l2))*(-(a4*D4*cosh(a3*(l2 - l3))) + a3*D3*sinh(a3*(l2 - l3))) + a2*D2*cosh(a2*(l1 - l2))*(a3*D3*cosh(a3*(l2 - l3)) - a4*D4*sinh(a3*(l2 - l3)))) + a2*D2*(exp(2*a1*l1) - exp(2*a1*z0))*(a3*D3*cosh(a2*(l1 - l2))*(a4*D4*cosh(a3*(l2 - l3)) - a3*D3*sinh(a3*(l2 - l3))) + a2*D2*sinh(a2*(l1 - l2))*(-(a3*D3*cosh(a3*(l2 - l3))) + a4*D4*sinh(a3*(l2 - l3))))))/(2.*a1*D1*(a1*D1*(1 + exp(2*a1*(l1 + zb)))*(a3*D3*sinh(a2*(l1 - l2))*(-(a4*D4*cosh(a3*(l2 - l3))) + a3*D3*sinh(a3*(l2 - l3))) + a2*D2*cosh(a2*(l1 - l2))*(a3*D3*cosh(a3*(l2 - l3)) - a4*D4*sinh(a3*(l2 - l3)))) + a2*D2*(-1 + exp(2*a1*(l1 + zb)))*(a3*D3*cosh(a2*(l1 - l2))*(a4*D4*cosh(a3*(l2 - l3)) - a3*D3*sinh(a3*(l2 - l3))) + a2*D2*sinh(a2*(l1 - l2))*(-(a3*D3*cosh(a3*(l2 - l3))) + a4*D4*sinh(a3*(l2 - l3))))))
-
-
-@jit
-def _phi_integrator(s, z, rho, z0, zb, ls, Ds, phi_func, alpha_args):
-    return s*j0(s*rho)*phi_func(s, z, z0, zb, ls, Ds, alpha_args)
-
-
-@jit
-def model_nlayer_ss(rho, mua, musp, depths, n, n_ext=1, int_limit=10, int_divs=10, eps=1e-16):
+def model_nlayer_ss(rho, mua, musp, depths, n, n_ext=1, int_limit=10, int_divs=10):
     """Model Steady-State Reflectance in N Layers with Extrapolated Boundary Condition.
     Source: "Noninvasive determination of the optical properties of two-layered turbid media"
     parameters:
@@ -64,36 +93,23 @@ def model_nlayer_ss(rho, mua, musp, depths, n, n_ext=1, int_limit=10, int_divs=1
         n_ext := External Index of Refraction []
         int_limit := Integration Limit [length]
         int_divs := Number of subregions to integrate over []
-        eps := Epsilion for numerical diffrention [length]
     """
     nlayer = len(mua)
     imp, refl_coeff, flu_coeff = gen_coeffs(n, n_ext)
     D1 = 1 / (3 * (mua[0] + musp[0]))
     z0 = 3*D1
     zb = 2*D1*imp
-    if nlayer == 2:
-        D2 = D1, 1 / (3 * (mua[1] + musp[1]))
-        alpha_args2 = mua[0] / D1, mua[1] / D2[1]
-        p_0 = integrate(_phi_integrator, 0, int_limit, int_divs, (0, rho, z0, zb, depths, D2, _2_layer_phi, alpha_args2)) / (2*pi)
-        p_eps = integrate(_phi_integrator, 0, int_limit, int_divs, (eps, rho, z0, zb, depths, D2, _2_layer_phi, alpha_args2)) / (2*pi)
-    elif nlayer == 3:
-        D3 = D1, 1 / (3 * (mua[1] + musp[1])), 1 / (3 * (mua[2] + musp[2]))
-        alpha_args3 = mua[0] / D1, mua[1] / D3[1], mua[2] / D3[2]
-        p_0 = integrate(_phi_integrator, 0, int_limit, int_divs, (0, rho, z0, zb, depths, D3, _3_layer_phi, alpha_args3)) / (2*pi)
-        p_eps = integrate(_phi_integrator, 0, int_limit, int_divs, (eps, rho, z0, zb, depths, D3, _3_layer_phi, alpha_args3)) / (2*pi)
-    elif nlayer == 4:
-        D4 = D1, 1 / (3 * (mua[1] + musp[1])), 1 / (3 * (mua[2] + musp[2])), 1 / (3 * (mua[3] + musp[3]))
-        alpha_args4 = mua[0] / D1, mua[1] / D4[1], mua[2] / D4[2], mua[3] / D4[3]
-        p_0 = integrate(_phi_integrator, 0, int_limit, int_divs, (0, rho, z0, zb, depths, D4, _4_layer_phi, alpha_args4)) / (2*pi)
-        p_eps = integrate(_phi_integrator, 0, int_limit, int_divs, (eps, rho, z0, zb, depths, D4, _4_layer_phi, alpha_args4)) / (2*pi)
-    else:
-        return model_ss(rho, mua[0], musp[0], n, n_ext)
-    dp = (p_eps - p_0) / eps
-    return flu_coeff*p_0 + refl_coeff*D1*dp
+    alpha_args = (mua,)
+    return integrate(_refl_integrator, 0, int_limit, int_divs, (0, rho, z0, zb, depths, mua, musp, _ss_alphas, alpha_args, flu_coeff, refl_coeff)) / (2*pi)
 
 
 @jit
-def model_nlayer_fd(rho, mua, musp, depths, freq, c, n, n_ext=1, int_limit=10, int_divs=10, eps=1e-16):
+def _fd_alphas(s, i, d, wave, muas):
+    return sqrt(s**2 + (wave + muas[i]) / d)
+
+
+@jit
+def model_nlayer_fd(rho, mua, musp, depths, freq, c, n, n_ext=1, int_limit=10, int_divs=10):
     """Model Frequncy-Domain Reflectance in N Layers with Extrapolated Boundary Condition.
     Source: "Noninvasive determination of the optical properties of two-layered turbid media"
     parameters:
@@ -107,7 +123,6 @@ def model_nlayer_fd(rho, mua, musp, depths, freq, c, n, n_ext=1, int_limit=10, i
         n_ext := External Index of Refraction []
         int_limit := Integration Limit [length]
         int_divs := Number of subregions to integrate over []
-        eps := Epsilion for numerical diffrention [length]
     """
     nlayer = len(mua)
     imp, refl_coeff, flu_coeff = gen_coeffs(n, n_ext)
@@ -117,30 +132,18 @@ def model_nlayer_fd(rho, mua, musp, depths, freq, c, n, n_ext=1, int_limit=10, i
     w = 2*pi*freq
     v = c / n
     wave = w/v*1j
-    if nlayer == 2:
-        D2 = D1, 1 / (3 * (mua[1] + musp[1]))
-        alpha_args2 = (wave + mua[0]) / D1, (wave + mua[1]) / D2[1]
-        p_0 = integrate(_phi_integrator, 0, int_limit, int_divs, (0, rho, z0, zb, depths, D2, _2_layer_phi, alpha_args2)) / (2*pi)
-        p_eps = integrate(_phi_integrator, 0, int_limit, int_divs, (eps, rho, z0, zb, depths, D2, _2_layer_phi, alpha_args2)) / (2*pi)
-    elif nlayer == 3:
-        D3 = D1, 1 / (3 * (mua[1] + musp[1])), 1 / (3 * (mua[2] + musp[2]))
-        alpha_args3 = (wave + mua[0]) / D1, (wave + mua[1]) / D3[1], (wave + mua[2]) / D3[2]
-        p_0 = integrate(_phi_integrator, 0, int_limit, int_divs, (0, rho, z0, zb, depths, D3, _3_layer_phi, alpha_args3)) / (2*pi)
-        p_eps = integrate(_phi_integrator, 0, int_limit, int_divs, (eps, rho, z0, zb, depths, D3, _3_layer_phi, alpha_args3)) / (2*pi)
-    elif nlayer == 4:
-        D4 = D1, 1 / (3 * (mua[1] + musp[1])), 1 / (3 * (mua[2] + musp[2])), 1 / (3 * (mua[3] + musp[3]))
-        alpha_args4 = (wave + mua[0]) / D1, (wave + mua[1]) / D4[1], (wave + mua[2]) / D4[2], (wave + mua[3]) / D4[3]
-        p_0 = integrate(_phi_integrator, 0, int_limit, int_divs, (0, rho, z0, zb, depths, D4, _4_layer_phi, alpha_args4)) / (2*pi)
-        p_eps = integrate(_phi_integrator, 0, int_limit, int_divs, (eps, rho, z0, zb, depths, D4, _4_layer_phi, alpha_args4)) / (2*pi)
-    else:
-        return model_fd(rho, mua[0], musp[0], n, n_ext, freq, c)
-    dp = (p_eps - p_0) / eps
-    return flu_coeff*p_0 + refl_coeff*D1*dp
+    alpha_args = wave, mua
+    return integrate(_refl_integrator, 0, int_limit, int_divs, (0, rho, z0, zb, depths, mua, musp, _fd_alphas, alpha_args, flu_coeff, refl_coeff)) / (2*pi)
 
 
 @jit
-def model_nlayer_g2(rho, tau, mua, musp, depths, BFi, wavelength, n, n_ext=1, beta=0.5, tau_0=0, int_limit=10, int_divs=10, eps=1e-16):
-    """Model g2 (autocorelation) for Diffuse Correlation Spectroscopy in 4 Layers with Extrapolated Boundary Condition.
+def _g2_alphas(s, i, d, muas, musps, k0, BFi, tau):
+    return sqrt(s**2 + (muas[i] + 2 * musps[i] * k0**2 * BFi[i] * tau) / d)
+
+
+@jit
+def model_nlayer_g2(rho, tau, mua, musp, depths, BFi, wavelength, n, n_ext=1, beta=0.5, tau_0=0, int_limit=10, int_divs=10):
+    """Model g2 (autocorelation) for Diffuse Correlation Spectroscopy in N Layers with Extrapolated Boundary Condition.
     Source1: "Noninvasive determination of the optical properties of two-layered turbid media"
     Source2: "Diffuse optics for tissue monitoring and tomography"
     parameters:
@@ -157,7 +160,6 @@ def model_nlayer_g2(rho, tau, mua, musp, depths, BFi, wavelength, n, n_ext=1, be
         tau_0 := The first tau for normalization [time]
         int_limit := Integration Limit [length]
         int_divs := Number of subregions to integrate over []
-        eps := Epsilion for numerical diffrention [length]
     """
     nlayer = len(mua)
     imp, refl_coeff, flu_coeff = gen_coeffs(n, n_ext)
@@ -165,33 +167,9 @@ def model_nlayer_g2(rho, tau, mua, musp, depths, BFi, wavelength, n, n_ext=1, be
     z0 = 3*D1
     zb = 2*D1*imp
     k0 = 2 * pi * n / wavelength
-    if nlayer == 2:
-        D2 = D1, 1/(3 * (mua[1] + musp[1]))
-        alpha_args2 = (mua[0]+2*musp[0]*k0**2*BFi[0]*tau)/D2[0], (mua[1]+2*musp[1]*k0**2*BFi[1]*tau)/D2[1]
-        alpha_norm_args2 = (mua[0]+2*musp[0]*k0**2*BFi[0]*tau_0)/D2[0], (mua[1]+2*musp[1]*k0**2*BFi[1]*tau_0)/D2[1]
-        p_0 = integrate(_phi_integrator, 0, int_limit, int_divs, (0, rho, z0, zb, depths, D2, _2_layer_phi, alpha_args2)) / (2*pi)
-        p_0_norm = integrate(_phi_integrator, 0, int_limit, int_divs, (0, rho, z0, zb, depths, D2, _2_layer_phi, alpha_norm_args2)) / (2*pi)
-        p_eps = integrate(_phi_integrator, 0, int_limit, int_divs, (eps, rho, z0, zb, depths, D2, _2_layer_phi, alpha_args2)) / (2*pi)
-        p_eps_norm = integrate(_phi_integrator, 0, int_limit, int_divs, (eps, rho, z0, zb, depths, D2, _2_layer_phi, alpha_norm_args2)) / (2*pi)
-    elif nlayer == 3:
-        D3 = D1, 1/(3 * (mua[1] + musp[1])), 1/(3 * (mua[2] + musp[2]))
-        alpha_args3 = (mua[0]+2*musp[0]*k0**2*BFi[0]*tau)/D3[0], (mua[1]+2*musp[1]*k0**2*BFi[1]*tau)/D3[1], (mua[2]+2*musp[2]*k0**2*BFi[2]*tau)/D3[2]
-        alpha_norm_args3 = (mua[0]+2*musp[0]*k0**2*BFi[0]*tau_0)/D3[0], (mua[1]+2*musp[1]*k0**2*BFi[1]*tau_0)/D3[1], (mua[2]+2*musp[2]*k0**2*BFi[2]*tau_0)/D3[2]
-        p_0 = integrate(_phi_integrator, 0, int_limit, int_divs, (0, rho, z0, zb, depths, D3, _3_layer_phi, alpha_args3)) / (2*pi)
-        p_0_norm = integrate(_phi_integrator, 0, int_limit, int_divs, (0, rho, z0, zb, depths, D3, _3_layer_phi, alpha_norm_args3)) / (2*pi)
-        p_eps = integrate(_phi_integrator, 0, int_limit, int_divs, (eps, rho, z0, zb, depths, D3, _3_layer_phi, alpha_args3)) / (2*pi)
-        p_eps_norm = integrate(_phi_integrator, 0, int_limit, int_divs, (eps, rho, z0, zb, depths, D3, _3_layer_phi, alpha_norm_args3)) / (2*pi)
-    elif nlayer == 4:
-        D4 = D1, 1/(3 * (mua[1] + musp[1])), 1/(3 * (mua[2] + musp[2])), 1/(3 * (mua[3] + musp[3]))
-        alpha_args4 = (mua[0]+2*musp[0]*k0**2*BFi[0]*tau)/D4[0], (mua[1]+2*musp[1]*k0**2*BFi[1]*tau)/D4[1], (mua[2]+2*musp[2]*k0**2*BFi[2]*tau)/D4[2], (mua[3]+2*musp[3]*k0**2*BFi[3]*tau)/D4[3]
-        alpha_norm_args4 = (mua[0]+2*musp[0]*k0**2*BFi[0]*tau_0)/D4[0], (mua[1]+2*musp[1]*k0**2*BFi[1]*tau_0)/D4[1], (mua[2]+2*musp[2]*k0**2*BFi[2]*tau_0)/D4[2], (mua[3]+2*musp[3]*k0**2*BFi[3]*tau_0)/D4[3]
-        p_0 = integrate(_phi_integrator, 0, int_limit, int_divs, (0, rho, z0, zb, depths, D4, _4_layer_phi, alpha_args4)) / (2*pi)
-        p_0_norm = integrate(_phi_integrator, 0, int_limit, int_divs, (0, rho, z0, zb, depths, D4, _4_layer_phi, alpha_norm_args4)) / (2*pi)
-        p_eps = integrate(_phi_integrator, 0, int_limit, int_divs, (eps, rho, z0, zb, depths, D4, _4_layer_phi, alpha_args4)) / (2*pi)
-        p_eps_norm = integrate(_phi_integrator, 0, int_limit, int_divs, (eps, rho, z0, zb, depths, D4, _4_layer_phi, alpha_norm_args4)) / (2*pi)
-    else:
-        return model_g2(tau, BFi[0], beta, mua[0], musp[0], wavelength, rho, tau_0, n, n_ext)
-    dp = (p_eps - p_0) / eps
-    dp_norm = (p_eps_norm - p_0_norm) / eps
-    g1 = (flu_coeff*p_0 + refl_coeff*D1*dp) / (flu_coeff*p_0_norm + refl_coeff*D1*dp_norm)
+    alpha_args = mua, musp, k0, BFi, tau
+    alpha_norm_args = mua, musp, k0, BFi, tau_0
+    refl = integrate(_refl_integrator, 0, int_limit, int_divs, (0, rho, z0, zb, depths, mua, musp, _g2_alphas, alpha_args, flu_coeff, refl_coeff))
+    refl_norm = integrate(_refl_integrator, 0, int_limit, int_divs, (0, rho, z0, zb, depths, mua, musp, _g2_alphas, alpha_norm_args, flu_coeff, refl_coeff))
+    g1 = refl / refl_norm
     return 1 + beta * g1 ** 2
