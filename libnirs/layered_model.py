0c63d1584c63359109658308ecde639cc5fb33f4
Julia Tatz <julia.tatz@childrens.harvard.edu>
2021-01-13 16:20:00 -0500

    nlayer now using PCBC & optionally using fft Now using the more accurate partial-current boundary condition over the less accurate extrapolated boundary condition for nlayer Optional usage of more accurate fft for Hankel Transform, instead of direct integration Added g1, for when g2 is not needed Refactored & improved homogenous impl

--- libnirs/layered_model.py
+++ libnirs/layered_model.py
@@ -1,6 +1,8 @@
+import numpy as np
 from numpy import pi, exp, sqrt, sinh, cosh
-from scipy.special import j0
-from .utils import jit, integrate, gen_coeffs
+from scipy import fft
+from scipy.special import j0, gamma
+from .utils import jit, integrate, gen_impedance
 from .model import model_ss, model_fd, model_g2
 
 """
@@ -11,24 +13,8 @@ memory allocation needed for storing the precumputed values isn't allowed
 for the cuda target.
 """
 
-"""
-d[i_]:=Indexed[\[ScriptCapitalD],i]
-a[i_]:=Sqrt[s^2+Indexed[\[Beta],i]/d[i]](*Indexed[\[Alpha],i]*)
-l[i_]:=Indexed[\[ScriptL],i]
-z0=Subscript[z,0];
-zb=Subscript[z,b];
-$Assumptions=z0>0&&zb>0;
-kn[1,n_]:=(a[n-1]d[n-1]-a[n]d[n])Exp[-l[n-1](a[n-1]+a[n])]
-kd[1,n_]:=(a[n-1]d[n-1]+a[n]d[n])Exp[+l[n-1](a[n-1]-a[n])]
-kn[i_,n_]:=(a[n-i]d[n-i]+a[n-i+1]d[n-i+1])Exp[-l[n-i](a[n-i]-a[n-i+1])]kn[i-1,n]+(a[n-i]d[n-i]-a[n-i+1]d[n-i+1])Exp[-l[n-i](a[n-i]+a[n-i+1])]kd[i-1,n]
-kd[i_,n_]:=(a[n-i]d[n-i]+a[n-i+1]d[n-i+1])Exp[+l[n-i](a[n-i]-a[n-i+1])]kn[i-1,n]+(a[n-i]d[n-i]-a[n-i+1]d[n-i+1])Exp[-l[n-i](a[n-i]+a[n-i+1])]kd[i-1,n]
-k[i_,n_]:=kn[n-i+1,n]/kd[n-i+1,n]
-coeff[n_]:=Exp[a[1]zb](a[1]d[1](1+Exp[2 l[1]a[2]]k[2,n])Cosh[a[1](l[1]-z0)]+a[2]d[2](1-Exp[2 l[1]a[2]]k[2,n])Sinh[a[1](l[1]-z0)]) / (2 a[1]d[1](a[1]d[1](1+Exp[2 l[1]a[2]]k[2,n])Cosh[a[1](l[1]+zb)]+a[2]d[2](1-Exp[2 l[1]a[2]]k[2,n])Sinh[a[1](l[1]+zb)]) )
-phi1[n_,z_]:=2 coeff[n]Exp[-a[1]zb]Sinh[a[1](zb+z)](*+HeavisideTheta[z-z0]Sinh[a[1](z-z0)]/(a[1]d[1])*)
-"""
-
 @jit
-def _n_layer_refl(s, z, z0, zb, ls, muas, musps, alphas, alpha_args, flu_coeff, refl_coeff):
+def _n_layer_refl(s, z0, zb, ls, muas, musps, alphas, alpha_args):
     n = len(muas)
 
     def ds(i):
@@ -41,12 +27,9 @@ def _n_layer_refl(s, z, z0, zb, ls, muas, musps, alphas, alpha_args, flu_coeff, 
 
     if n == 2:
         l1 = ls[0]
-        coeff_n = d1 * alpha1 * cosh(alpha1 * (l1 - z0)) + d2 * alpha2 * sinh(alpha1 * (l1 - z0))
-        coeff_d = d1 * (d1 * alpha1 * cosh(alpha1 * (l1 + zb)) + d2 * alpha2 * sinh(alpha1 * (l1 + zb)))
-        coeff = coeff_n / coeff_d
-        phi = coeff * sinh(alpha1 * (z + zb)) / alpha1
-        dz_phi = coeff * cosh(alpha1 * (z + zb))
-        return flu_coeff * phi + refl_coeff * d1 * dz_phi
+        signal = (cosh((-z0 + l1)*alpha1)*d1*alpha1 + sinh((-z0 + l1)*alpha1)*d2*alpha2) / \
+            (d1*alpha1*(cosh(l1*alpha1) + zb*sinh(l1*alpha1)*alpha1) + d2*(sinh(l1*alpha1) + zb*cosh(l1*alpha1)*alpha1)*alpha2)
+        return signal
 
     k_num = (alpha1 * d1 - alpha2 * d2) * exp(-ls[-1] * (alpha1 + alpha2))
     k_dem = (alpha1 * d1 + alpha2 * d2) * exp( ls[-1] * (alpha1 - alpha2))
@@ -65,31 +48,47 @@ def _n_layer_refl(s, z, z0, zb, ls, muas, musps, alphas, alpha_args, flu_coeff, 
         d1, d2 = ds(i-1), d1
         alpha1, alpha2 = alphas(s, i - 1, d1, *alpha_args), alpha1
     l1 = ls[0]
-    coeff = (
-        exp(zb*alpha1) * (
-            alpha1 * d1 * (exp(2 * l1 *alpha2) * k_num + k_dem) *
-            cosh(alpha1*(l1-z0)) +
-            alpha2 * d2 * (-exp(2 * l1 *alpha2) * k_num + k_dem) *
-            sinh(alpha1*(l1-z0))
-        ) / (
-            2 * alpha1 * d1 * (
-                alpha1 * d1 * (exp(2 * l1 *alpha2) * k_num + k_dem) *
-                cosh(alpha1*(l1+zb)) +
-                alpha2 * d2 * (-exp(2 * l1 *alpha2) * k_num + k_dem) *
-                sinh(alpha1*(l1+zb))
-            )
-        )
-    )
-
-    phi = 2 * coeff * exp(-alpha1 * zb) * sinh(alpha1 * (zb + z))
-    dz_phi = 2 * alpha1 * coeff * exp(-alpha1 * zb) * cosh(alpha1 * (zb + z))
-    return flu_coeff * phi + refl_coeff * d1 * dz_phi
+    signal = (cosh((-z0 + l1)*alpha1)*(k_dem + exp(2*l1*alpha2)*k_num)*d1*alpha1 + (k_dem - exp(2*l1*alpha2)*k_num)*sinh((-z0 + l1)*alpha1)*d2*alpha2) / \
+        ((k_dem + exp(2*l1*alpha2)*k_num)*d1*alpha1*(cosh(l1*alpha1) + zb*sinh(l1*alpha1)*alpha1) + (k_dem - exp(2*l1*alpha2)*k_num)*d2*(sinh(l1*alpha1) + zb*cosh(l1*alpha1)*alpha1)*alpha2)
+    return signal
 
 
 @jit
-def _refl_integrator(s, z, rho, z0, zb, ls, muas, musps, alphas, alpha_args, flu_coeff, refl_coeff):
+def _refl_integrator(s, rho, z0, zb, ls, muas, musps, alphas, alpha_args):
     """Inverse HankelTransform"""
-    return s*j0(s*rho)*_n_layer_refl(s, z, z0, zb, ls, muas, musps, alphas, alpha_args, flu_coeff, refl_coeff)
+    return s*j0(s*rho)*_n_layer_refl(s, z0, zb, ls, muas, musps, alphas, alpha_args)
+
+
+def fourierBesselJv(omega):
+    return gamma((1 - 1j * omega) / 2) / gamma((1 + 1j * omega) / 2) * 2**(-1j * omega)
+
+
+def rfft_hankel(integrator, integrator_args, log_limit, npoints):
+    """Inverse HankelTransform using Fast Fourier Transform"""
+    sp = np.linspace(-log_limit, log_limit, fft.next_fast_len(npoints, True))
+    dt = sp[1] - sp[0]
+    s = np.exp(-sp)
+    r = np.exp(sp)
+    res = s * np.nan_to_num(integrator(s, *integrator_args))
+    wq = 2 * pi * fft.rfftfreq(len(sp), dt)
+    fres = fft.rfft(fft.ifftshift(res), norm="ortho") * dt
+    fcres = fourierBesselJv(wq) * fres
+    hres = fft.ifftshift(fft.irfft(fcres, norm="ortho")) * sp.size * (wq[1] - wq[0]) / (2*pi)
+    return r, hres / r
+
+
+def fft_hankel(integrator, integrator_args, log_limit, npoints):
+    """Inverse HankelTransform using Fast Fourier Transform"""
+    sp = np.linspace(-log_limit, log_limit, fft.next_fast_len(npoints, True))
+    dt = sp[1] - sp[0]
+    s = np.exp(-sp)
+    r = np.exp(sp)
+    res = s * np.nan_to_num(integrator(s, *integrator_args))
+    wq = 2 * pi * fft.fftfreq(len(sp), dt)
+    fres = fft.fft(fft.ifftshift(res), norm="ortho") * dt
+    fcres = fourierBesselJv(wq) * fres
+    hres = fft.ifftshift(fft.ifft(fcres, norm="ortho")) * sp.size * (wq[1] - wq[0]) / (2*pi)
+    return r, hres / r
 
 
 @jit
@@ -99,7 +98,7 @@ def _ss_alphas(s, i, d, muas):
 
 @jit
 def model_nlayer_ss(rho, mua, musp, depths, n, n_ext=1, int_limit=10, int_divs=10):
-    """Model Steady-State Reflectance in N Layers with Extrapolated Boundary Condition.
+    """Model Steady-State Reflectance in N Layers with Partial-Current Boundary Condition.
     Source: "Noninvasive determination of the optical properties of two-layered turbid media"
     parameters:
         rho := Source-Detector Seperation [length]
@@ -112,13 +111,37 @@ def model_nlayer_ss(rho, mua, musp, depths, n, n_ext=1, int_limit=10, int_divs=1
         int_divs := Number of subregions to integrate over []
     """
     nlayer = len(mua)
-    imp, refl_coeff, flu_coeff = gen_coeffs(n, n_ext)
+    imp = gen_impedance(n/n_ext)
     D1 = 1 / (3 * (mua[0] + musp[0]))
     z0 = 3*D1
     assert depths[0] >= z0
     zb = 2*D1*imp
     alpha_args = (mua,)
-    return integrate(_refl_integrator, 0, int_limit, int_divs, (0, rho, z0, zb, depths, mua, musp, _ss_alphas, alpha_args, flu_coeff, refl_coeff)) / (2*pi)
+    return integrate(_refl_integrator, 0, int_limit, int_divs, (rho, z0, zb, depths, mua, musp, _ss_alphas, alpha_args)) / (2*pi)
+
+
+def model_nlayer_ss_fft(rho, mua, musp, depths, n, n_ext=1, log_limit=15, npoints=512):
+    """Model Steady-State Reflectance in N Layers with Partial-Current Boundary Condition.
+    Source: "Noninvasive determination of the optical properties of two-layered turbid media"
+    parameters:
+        rho := Source-Detector Seperation [length]
+        mua := N Absorption Coefficents [1/length]
+        musp := N Reduced Scattering Coefficents [1/length]
+        depths := N-1 Layer Depths
+        n := Media Index of Refraction []
+        n_ext := External Index of Refraction []
+        log_limit := Log-Space Integration Limit for fft []
+        npoints := Minimum number of points to evalulate for fft []
+    """
+    nlayer = len(mua)
+    imp = gen_impedance(n / n_ext)
+    D1 = 1 / (3 * (mua[0] + musp[0]))
+    z0 = 3*D1
+    assert depths[0] >= z0
+    zb = 2*D1*imp
+    alpha_args = (mua,)
+    r, h = rfft_hankel(_n_layer_refl, (z0, zb, depths, mua, musp, _ss_alphas, alpha_args), log_limit, npoints)
+    return np.exp(np.interp(rho, r, np.log(h))) / (2*pi)
 
 
 @jit
@@ -128,7 +151,7 @@ def _fd_alphas(s, i, d, wave, muas):
 
 @jit
 def model_nlayer_fd(rho, mua, musp, depths, freq, c, n, n_ext=1, int_limit=10, int_divs=10):
-    """Model Frequncy-Domain Reflectance in N Layers with Extrapolated Boundary Condition.
+    """Model Frequncy-Domain Reflectance in N Layers with Partial-Current Boundary Condition.
     Source: "Noninvasive determination of the optical properties of two-layered turbid media"
     parameters:
         rho := Source-Detector Seperation [length]
@@ -143,7 +166,7 @@ def model_nlayer_fd(rho, mua, musp, depths, freq, c, n, n_ext=1, int_limit=10, i
         int_divs := Number of subregions to integrate over []
     """
     nlayer = len(mua)
-    imp, refl_coeff, flu_coeff = gen_coeffs(n, n_ext)
+    imp = gen_impedance(n/n_ext)
     D1 = 1 / (3 * (mua[0] + musp[0]))
     z0 = 3*D1
     assert depths[0] >= z0
@@ -152,17 +175,113 @@ def model_nlayer_fd(rho, mua, musp, depths, freq, c, n, n_ext=1, int_limit=10, i
     v = c / n
     wave = w/v*1j
     alpha_args = wave, mua
-    return integrate(_refl_integrator, 0, int_limit, int_divs, (0, rho, z0, zb, depths, mua, musp, _fd_alphas, alpha_args, flu_coeff, refl_coeff)) / (2*pi)
+    return integrate(_refl_integrator, 0, int_limit, int_divs, (rho, z0, zb, depths, mua, musp, _fd_alphas, alpha_args)) / (2*pi)
+
+
+def model_nlayer_fd_fft(rho, mua, musp, depths, freq, c, n, n_ext=1, log_limit=15, npoints=512):
+    """Model Frequncy-Domain Reflectance in N Layers with Partial-Current Boundary Condition.
+    Source: "Noninvasive determination of the optical properties of two-layered turbid media"
+    parameters:
+        rho := Source-Detector Seperation [length]
+        mua := N Absorption Coefficents [1/length]
+        musp := N Reduced Scattering Coefficents [1/length]
+        depths := N-1 Layer Depths
+        freq := Frequncy of Source [1/time]
+        c := Speed of Light in vacuum [length/time]
+        n := Media Index of Refraction []
+        n_ext := External Index of Refraction []
+        log_limit := Log-Space Integration Limit for fft []
+        npoints := Minimum number of points to evalulate for fft []
+    """
+    nlayer = len(mua)
+    imp = gen_impedance(n/n_ext)
+    D1 = 1 / (3 * (mua[0] + musp[0]))
+    z0 = 3*D1
+    assert depths[0] >= z0
+    zb = 2*D1*imp
+    w = 2*pi*freq
+    v = c / n
+    wave = w/v*1j
+    alpha_args = wave, mua
+    r, h = fft_hankel(_n_layer_refl, (z0, zb, depths, mua, musp, _fd_alphas, alpha_args), log_limit, npoints)
+    return np.exp(np.interp(rho, r, np.log(h))) / (2*pi)
 
 
 @jit
-def _g2_alphas(s, i, d, muas, musps, k0, BFi, tau):
+def _g1_alphas(s, i, d, muas, musps, k0, BFi, tau):
     return sqrt(s**2 + (muas[i] + 2 * musps[i] * k0**2 * BFi[i] * tau) / d)
 
 
 @jit
+def model_nlayer_g1(rho, tau, mua, musp, depths, BFi, wavelength, n, n_ext=1, tau_0=0, int_limit=10, int_divs=10):
+    """Model g1 (autocorelation) for Diffuse Correlation Spectroscopy in N Layers with Partial-Current Boundary Condition.
+    Source1: "Noninvasive determination of the optical properties of two-layered turbid media"
+    Source2: "Diffuse optics for tissue monitoring and tomography"
+    parameters:
+        rho := Source-Detector Seperation [length]
+        tau := Correlation Time [time]
+        mua := N Absorption Coefficents [1/length]
+        musp := N Reduced Scattering Coefficents [1/length]
+        depths := N-1 Layer Depths
+        BFi := N Blood Flow indices []
+        wavelength := Measurement Wavelength [length]
+        n := Media Index of Refraction []
+        n_ext := External Index of Refraction []
+        tau_0 := The first tau for normalization [time]
+        int_limit := Integration Limit [length]
+        int_divs := Number of subregions to integrate over []
+    """
+    nlayer = len(mua)
+    imp = gen_impedance(n/n_ext)
+    D1 = 1 / (3 * (mua[0] + musp[0]))
+    z0 = 3*D1
+    assert depths[0] >= z0
+    zb = 2*D1*imp
+    k0 = 2 * pi * n / wavelength
+    alpha_args = mua, musp, k0, BFi, tau
+    alpha_norm_args = mua, musp, k0, BFi, tau_0
+    refl = integrate(_refl_integrator, 0, int_limit, int_divs, (rho, z0, zb, depths, mua, musp, _g1_alphas, alpha_args))
+    refl_norm = integrate(_refl_integrator, 0, int_limit, int_divs, (rho, z0, zb, depths, mua, musp, _g1_alphas, alpha_norm_args))
+    g1 = refl / refl_norm
+    return g1
+
+
+def model_nlayer_g1_fft(rho, tau, mua, musp, depths, BFi, wavelength, n, n_ext=1, tau_0=0, log_limit=15, npoints=512):
+    """Model g1 (autocorelation) for Diffuse Correlation Spectroscopy in N Layers with Partial-Current Boundary Condition.
+    Source1: "Noninvasive determination of the optical properties of two-layered turbid media"
+    Source2: "Diffuse optics for tissue monitoring and tomography"
+    parameters:
+        rho := Source-Detector Seperation [length]
+        tau := Correlation Time [time]
+        mua := N Absorption Coefficents [1/length]
+        musp := N Reduced Scattering Coefficents [1/length]
+        depths := N-1 Layer Depths [length]
+        BFi := N Blood Flow indices []
+        wavelength := Measurement Wavelength [length]
+        n := Media Index of Refraction []
+        n_ext := External Index of Refraction []
+        tau_0 := The first tau for normalization [time]
+        log_limit := Log-Space Integration Limit for fft []
+        npoints := Minimum number of points to evalulate for fft []
+    """
+    nlayer = len(mua)
+    imp = gen_impedance(n/n_ext)
+    D1 = 1 / (3 * (mua[0] + musp[0]))
+    z0 = 3*D1
+    assert depths[0] >= z0
+    zb = 2*D1*imp
+    k0 = 2 * pi * n / wavelength
+    alpha_args = mua, musp, k0, BFi, tau
+    alpha_norm_args = mua, musp, k0, BFi, tau_0
+    r, h = rfft_hankel(_n_layer_refl, (z0, zb, depths, mua, musp, _g1_alphas, alpha_args), log_limit, npoints)
+    _r, h_norm = rfft_hankel(_n_layer_refl, (z0, zb, depths, mua, musp, _g1_alphas, alpha_norm_args), log_limit, npoints)
+    g1 = np.interp(rho, r, (h / h_norm))
+    return g1
+
+
+@jit
 def model_nlayer_g2(rho, tau, mua, musp, depths, BFi, wavelength, n, n_ext=1, beta=0.5, tau_0=0, int_limit=10, int_divs=10):
-    """Model g2 (autocorelation) for Diffuse Correlation Spectroscopy in N Layers with Extrapolated Boundary Condition.
+    """Model g2 (autocorelation) for Diffuse Correlation Spectroscopy in N Layers with Partial-Current Boundary Condition.
     Source1: "Noninvasive determination of the optical properties of two-layered turbid media"
     Source2: "Diffuse optics for tissue monitoring and tomography"
     parameters:
@@ -180,16 +299,5 @@ def model_nlayer_g2(rho, tau, mua, musp, depths, BFi, wavelength, n, n_ext=1, be
         int_limit := Integration Limit [length]
         int_divs := Number of subregions to integrate over []
     """
-    nlayer = len(mua)
-    imp, refl_coeff, flu_coeff = gen_coeffs(n, n_ext)
-    D1 = 1 / (3 * (mua[0] + musp[0]))
-    z0 = 3*D1
-    assert depths[0] >= z0
-    zb = 2*D1*imp
-    k0 = 2 * pi * n / wavelength
-    alpha_args = mua, musp, k0, BFi, tau
-    alpha_norm_args = mua, musp, k0, BFi, tau_0
-    refl = integrate(_refl_integrator, 0, int_limit, int_divs, (0, rho, z0, zb, depths, mua, musp, _g2_alphas, alpha_args, flu_coeff, refl_coeff))
-    refl_norm = integrate(_refl_integrator, 0, int_limit, int_divs, (0, rho, z0, zb, depths, mua, musp, _g2_alphas, alpha_norm_args, flu_coeff, refl_coeff))
-    g1 = refl / refl_norm
-    return 1 + beta * g1 ** 2
+    g1 = model_nlayer_g2(rho, tau, mua, musp, depths, BFi, wavelength, n, n_ext, tau_0, int_limit, int_divs)
+    return 1 + beta * g1**2
--- libnirs/model.py
+++ libnirs/model.py
@@ -3,6 +3,25 @@ from .utils import jit, gen_impedance, gen_coeffs
 
 
 @jit
+def _ecbc(k, rho, D, n, n_ext):
+    """Time-Independent Reflectance with Extrapolated Boundary Condition"""
+    imp, fresTj, fresTphi = gen_coeffs(n, n_ext)
+    # z = 0
+    z0 = 3 * D
+    zb = 2 * D * imp
+    # r1 = sqrt(rho ** 2 + (z - z0) ** 2)
+    r1 = sqrt(rho ** 2 + z0 ** 2)
+    # r2 = sqrt(rho ** 2 + (z + z0 + 2 * zb) ** 2)
+    r2 = sqrt(rho ** 2 + (z0 + 2 * zb) ** 2)
+    phi = (exp(-k * r1) / r1 - exp(-k * r2) / r2) / (4 * pi * D)
+    j = (
+        z0 * (1 + k * r1) * exp(-k * r1) / r1 ** 3
+        + (z0 + 2 * zb) * (1 + k * r2) * exp(-k * r2) / r2 ** 3
+    ) / (4 * pi)
+    return fresTphi * phi + fresTj * j
+
+
+@jit
 def model_ss(rho, mua, musp, n, n_ext):
     """Model Steady-State Reflectance with Extrapolated Boundary Condition.
     Source: "Improved solutions of the steady-state and the time-resolved diffusion equations for reflectance from a semi-infinite turbid medium"
@@ -13,19 +32,9 @@ def model_ss(rho, mua, musp, n, n_ext):
         n := Media Index of Refraction []
         n_ext := External Index of Refraction []
     """
-    imp, refl_coeff, flu_coeff = gen_coeffs(n, n_ext)
     D = 1 / (3 * (mua + musp))
-    z = 0
-    z0 = 1 / (mua + musp)
-    zb = 2 * D * imp
-    mu_eff = sqrt(3 * mua * (mua + musp))
-    r1_sq = (z - z0)**2 + rho**2
-    r1 = sqrt(r1_sq)
-    r2_sq = (z + z0 + 2*zb)**2 + rho**2
-    r2 = sqrt(r2_sq)
-    flu_rate = 1/(4*pi*D)*(exp(-mu_eff*r1)/r1 - exp(-mu_eff*r2)/r2)
-    diff_refl = 1/(4*pi)*(z0*(mu_eff + 1/r1)*exp(-mu_eff*r1)/r1_sq + (z0 + 2*zb)*(mu_eff + 1/r2)*exp(-mu_eff*r2)/r2_sq)
-    return flu_coeff * flu_rate + refl_coeff * diff_refl
+    mu_eff = sqrt(mua / D)
+    return _ecbc(mu_eff, rho, D, n, n_ext)
 
 
 @jit
@@ -41,22 +50,12 @@ def model_fd(rho, mua, musp, n, n_ext, freq, c):
         freq := Frequncy of Source [1/time]
         c := Speed of Light in vacuum [length/time]
     """
-    imp, refl_coeff, flu_coeff = gen_coeffs(n, n_ext)
     D = 1 / (3 * (mua + musp))
-    z = 0
-    z0 = 1 / (mua + musp)
-    zb = 2 * D * imp
     v = c / n
-    w = 2 * pi * freq
-    k_in = sqrt(1+(w/(mua*v))**2)
-    k = sqrt(3 / 2 * mua * (mua + musp)) * (sqrt(k_in+1) + 1j*sqrt(k_in-1))
-    r1_sq = (z - z0)**2 + rho**2
-    r1 = sqrt(r1_sq)
-    r2_sq = (z + z0 + 2*zb)**2 + rho**2
-    r2 = sqrt(r2_sq)
-    flu_rate = 1/(4*pi*D)*(exp(-k*r1)/r1 - exp(-k*r2)/r2)
-    diff_refl = 1/(4*pi)*(z0*(k + 1/r1)*exp(-k*r1)/r1_sq + (z0 + 2*zb)*(k + 1/r2)*exp(-k*r2)/r2_sq)
-    return flu_coeff * flu_rate + refl_coeff * diff_refl
+    omega = 2 * pi * freq
+    k_in = sqrt(1 + (omega / (mua * v)) ** 2)
+    k = sqrt(mua / D / 2) * (sqrt(k_in + 1) + 1j * sqrt(k_in - 1))
+    return _ecbc(k, rho, D, n, n_ext)
 
 
 @jit
@@ -76,13 +75,49 @@ def model_td(t, rho, mua, musp, n, n_ext, c):
     v = c / n  # Speed of light in turbid medium
     D = 1 / (3 * (mua + musp))  # diffusion constant
     z = 0
-    z0 = 1 / (mua + musp)
-    zb = 2 * imp * D
-    r1_sq = (z - z0)**2 + rho**2
-    r2_sq = (z + z0 + 2*zb)**2 + rho**2
-    reflectance = 0.5*t**(-5/2)*(4*pi*D*v)**(-3/2)*exp(-mua*v*t)*(z0*exp(-r1_sq/(4*D*v*t))+(z0+2*zb)*exp(-r2_sq/(4*D*v*t)))
-    fluence_rate = v*(4*pi*D*v*t)**(-3/2)*exp(-mua*v*t)*(exp(-(r1_sq/(4*D*v*t)))-exp(-(r2_sq/(4*D*v*t))))
-    return flu_coeff*fluence_rate + refl_coeff*reflectance
+    z0 = 3 * D
+    zb = 2 * D * imp
+    r1_sq = (z - z0) ** 2 + rho ** 2
+    r2_sq = (z + z0 + 2 * zb) ** 2 + rho ** 2
+    reflectance = (
+        0.5
+        * t ** (-5 / 2)
+        * (4 * pi * D * v) ** (-3 / 2)
+        * exp(-mua * v * t)
+        * (
+            z0 * exp(-r1_sq / (4 * D * v * t))
+            + (z0 + 2 * zb) * exp(-r2_sq / (4 * D * v * t))
+        )
+    )
+    fluence_rate = (
+        v
+        * (4 * pi * D * v * t) ** (-3 / 2)
+        * exp(-mua * v * t)
+        * (exp(-(r1_sq / (4 * D * v * t))) - exp(-(r2_sq / (4 * D * v * t))))
+    )
+    return flu_coeff * fluence_rate + refl_coeff * reflectance
+
+
+@jit
+def model_g1(tau, bfi, mua, musp, wavelength, rho, first_tau_delay, n, n_ext=1):
+    """Model g1 (autocorelation) for Diffuse correlation spectroscopy.
+    Source: "Diffuse optics for tissue monitoring and tomography"
+    parameters:
+        tau := Correlation time [time]
+        bfi := Blood-Flow Index [1/length^2/time]
+        mua := Absorption Coefficent [1/length]
+        musp := Reduced Scattering Coefficent [1/length]
+        wavelength := Wavelength of Light [length]
+        rho := Source-Detector Seperation [length]
+        first_tau_delay := The first tau for normalization [time]
+        n := Media Index of Refraction []
+        n_ext := External Index of Refraction []
+    """
+    D = 1 / (3 * (mua + musp))
+    k0 = 2 * pi * n / wavelength
+    k_tau = sqrt((mua + 2 * musp * k0 ** 2 * bfi * tau) / D)
+    k_norm = sqrt((mua + 2 * musp * k0 ** 2 * bfi * first_tau_delay) / D)
+    return _ecbc(k_tau, rho, D, n, n_ext) / _ecbc(k_norm, rho, D, n, n_ext)
 
 
 @jit
@@ -91,7 +126,7 @@ def model_g2(tau, bfi, beta, mua, musp, wavelength, rho, first_tau_delay, n, n_e
     Source: "Diffuse optics for tissue monitoring and tomography"
     parameters:
         tau := Correlation time [time]
-        bfi := Blood-Flow Index []
+        bfi := Blood-Flow Index [1/length^2/time]
         beta := Beta derived for Siegert relation []
         mua := Absorption Coefficent [1/length]
         musp := Reduced Scattering Coefficent [1/length]
@@ -101,15 +136,5 @@ def model_g2(tau, bfi, beta, mua, musp, wavelength, rho, first_tau_delay, n, n_e
         n := Media Index of Refraction []
         n_ext := External Index of Refraction []
     """
-    imp = gen_impedance(n / n_ext)
-    D = 1 / (3 * (mua + musp))  # diffusion constant
-    z = 0
-    z0 = 1 / (mua + musp)
-    zb = 2 * imp * D
-    k0 = 2 * pi * n / wavelength
-    k_tau = sqrt(3 * mua * musp + musp**2 * k0**2 * 6 * bfi * tau)
-    k_norm = sqrt(3 * mua * musp + musp**2 * k0**2 * 6 * bfi * first_tau_delay)
-    r1 = sqrt(rho**2 + (z - z0)**2)
-    r2 = sqrt(rho**2 + (z + z0 + 2 * zb)**2)
-    g1 = (exp(-k_tau*r1)/r1 - exp(-k_tau*r2)/r2) / (exp(-k_norm*r1)/r1 - exp(-k_norm*r2)/r2)
+    g1 = model_g1(tau, bfi, mua, musp, wavelength, rho, first_tau_delay, n, n_ext)
     return 1 + beta * g1 ** 2
