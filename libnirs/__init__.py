d3acd4c7edd5bf64bbf5f5ca71bbdb6e67102ec2
Julia Tatz <julia.tatz@childrens.harvard.edu>
2021-02-19 17:23:05 -0500

    Better extc coeff fns & pyproject.toml Removed unused functions

--- libnirs/__init__.py
+++ libnirs/__init__.py
@@ -1,7 +1,6 @@
 import libnirs.overrides
 from .utils import integrate
-from .extinction_coeffs import get_extinction_coeffs
+from .extinction_coeffs import water, oxyhemoglobin, deoxyhemoglobin, soy_oil, get_extinction_coeffs
 from .model import model_ss, model_fd, model_td, model_g1, model_g2
 from .layered_model import model_nlayer_ss, model_nlayer_fd, model_nlayer_g1, model_nlayer_g2, model_nlayer_ss_fft, model_nlayer_fd_fft, model_nlayer_g1_fft
 from .cuda import cuda_model_ss, cuda_model_fd, cuda_model_td, cuda_model_g2, cuda_model_nlayer_ss, cuda_model_nlayer_fd, cuda_model_nlayer_g1
-from .qrs import detect_qrs
--- libnirs/extinction_coeffs.py
+++ libnirs/extinction_coeffs.py
@@ -5,6 +5,27 @@ from scipy.interpolate import interp1d
 _extinctions = np.load(os.path.join(os.path.dirname(__file__), 'extc.npz'))
 _extinction_interps = {k: interp1d(*v) for k, v in _extinctions.items()}
 
+
+def water(wavelength: "Quantity[ArrayLike, 'nm']") -> "Quantity[ndarray, 1 / 'mm']":
+    '''Get Extinction Coefficent [1/fraction/mm] of water at given wavelength(s) [nm]'''
+    return _extinction_interps["water"](wavelength)
+
+
+def oxyhemoglobin(wavelength: "Quantity[ArrayLike, 'nm']") -> "Quantity[ndarray, 1 / 'molar' / 'mm']":
+    '''Get Extinction Coefficent [1/Molar/mm] of oxyhemoglobin [HbO] at given wavelength(s) [nm]'''
+    return _extinction_interps["HbO"](wavelength)
+
+
+def deoxyhemoglobin(wavelength: "Quantity[ArrayLike, 'nm']") -> "Quantity[ndarray, 1 / 'molar' / 'mm']":
+    '''Get Extinction Coefficent [1/Molar/mm] of deoxyhemoglobin [HbR] at given wavelength(s) [nm]'''
+    return _extinction_interps["HbR"](wavelength)
+
+
+def soy_oil(wavelength: "Quantity[ArrayLike, 'nm']") -> "Quantity[ndarray, 1 / 'mm']":
+    '''Get Extinction Coefficent [1/fraction/mm] of soy-oil at given wavelength(s) [nm]'''
+    return _extinction_interps["soyoil"](wavelength)
+
+
 def get_extinction_coeffs(wavelengths, *species, axis=0):
     """Get Extinction Coefficents of species at various wavelengths in an array.
     Units:
@@ -13,9 +34,11 @@ def get_extinction_coeffs(wavelengths, *species, axis=0):
         HbR := [1/Molar/mm]
         soyoil := [1/fraction/mm]
     """
+    import warnings
+    warnings.warn("Function `get_extinction_coeffs` is now deprecated!", category=warnings.DeprecationWarning, stacklevel=2)
     if len(species) == 0:
         raise Exception("No species given.")
     elif len(species) == 1:
         return _extinction_interps[species[0]](wavelengths)
     else:
-        return np.stack((_extinction_interps[k](wavelengths) for k in species), axis=axis)
+        return np.stack([_extinction_interps[k](wavelengths) for k in species], axis=axis)
