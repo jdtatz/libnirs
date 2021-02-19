62e21e51c2fb3bda9fc508ab572a9398b3f4183a
Julia Tatz <tatz.j@northeastern.edu>
2021-02-19 17:44:35 -0500

    Vectorized layered fft model, added precomputation Some clean-up

--- libnirs/__init__.py
+++ libnirs/__init__.py
@@ -1,5 +1,5 @@
 import libnirs.overrides
-from .utils import integrate
+from .utils import fma, integrate, gen_impedance, gen_coeffs, qrng
 from .extinction_coeffs import water, oxyhemoglobin, deoxyhemoglobin, soy_oil, get_extinction_coeffs
 from .model import model_ss, model_fd, model_td, model_g1, model_g2
 from .layered_model import model_nlayer_ss, model_nlayer_fd, model_nlayer_g1, model_nlayer_g2, model_nlayer_ss_fft, model_nlayer_fd_fft, model_nlayer_g1_fft
