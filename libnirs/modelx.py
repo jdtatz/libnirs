3403f533e3579c4c85b836294cf2ea8462d42f53
Julia Tatz <julia.tatz@childrens.harvard.edu>
2025-01-31 19:23:28 -0500

    jax models: consistent param order & spatial_freq for fd models
    There is no usage of jax model fns, so theres no need to care about backcompat

--- libnirs/modelx.py
+++ libnirs/modelx.py
@@ -120,22 +120,20 @@ def model_ss(rho, mua, musp, n_media, n_ext):
     return ecbc_reflectance(rho, k, mua, musp, n_media, n_ext)
 
 
-def model_fd(rho, mua, musp, n_media, n_ext, freq, c):
+def model_fd(rho, mua, musp, spatial_freq, n_media, n_ext):
     """Model Frequncy-Domain Reflectance with Extrapolated Boundary Condition.
     Source: "Kienle, A., & Patterson, M. S. (1997). Improved solutions of the steady-state and the time-resolved diffusion equations for reflectance from a semi-infinite turbid medium. Journal of the Optical Society of America A, 14(1), 246. doi:10.1364/josaa.14.000246 "
     parameters:
         rho := Source-Detector Seperation [length]
         mua := Absorption Coefficent [1/length]
         musp := Reduced Scattering Coefficent [1/length]
+        spatial_freq := Spatial Frequncy of the Source in vacuum (`ξ = freq / c`) [1/length]
         n_media := Media Index of Refraction []
         n_ext := External Index of Refraction []
-        freq := Frequncy of Source [1/time]
-        c := Speed of Light in vacuum [length/time]
     """
     D = 1 / (3 * (mua + musp))
-    v = c / n_media
-    omega = 2 * pi * freq
-    k = sqrt((mua + 1j * (omega / v)) / D)
+    src_k = 2 * pi * n_media * spatial_freq
+    k = sqrt((mua + 1j * src_k) / D)
     # k_in = sqrt(1 + (omega / (mua * v)) ** 2)
     # _k = sqrt(mua / D / 2) * (sqrt(k_in + 1) + 1j * sqrt(k_in - 1))
     # assert _k == k
@@ -160,16 +158,16 @@ def model_td(t, rho, mua, musp, n_media, n_ext, c):
     return pcbc_td_reflectance(t, rho, k, mua, musp, n_media, n_ext, c)
 
 
-def model_g1_unnorm(tau, bfi, mua, musp, wavelength, rho, n_media, n_ext):
+def model_g1_unnorm(rho, mua, musp, wavelength, bfi, tau, n_media, n_ext):
     """Model G1 (unnormalized electric field autocorrelation) for Diffuse correlation spectroscopy with Extrapolated Boundary Condition.
     Source: "Diffuse optics for tissue monitoring and tomography"
     parameters:
-        tau := Correlation time [time]
-        bfi := Blood-Flow Index [length^2/time]
+        rho := Source-Detector Seperation [length]
         mua := Absorption Coefficent [1/length]
         musp := Reduced Scattering Coefficent [1/length]
         wavelength := Wavelength of Light [length]
-        rho := Source-Detector Seperation [length]
+        bfi := Blood-Flow Index [length^2/time]
+        tau := Correlation time [time]
         n_media := Media Index of Refraction []
         n_ext := External Index of Refraction []
     """
@@ -179,39 +177,37 @@ def model_g1_unnorm(tau, bfi, mua, musp, wavelength, rho, n_media, n_ext):
     return ecbc_reflectance(rho, k, mua, musp, n_media, n_ext)
 
 
-def model_fd_g1_unnorm(tau, bfi, mua, musp, wavelength, rho, n_media, n_ext, freq, c):
+def model_fd_g1_unnorm(rho, mua, musp, wavelength, bfi, tau, spatial_freq, n_media, n_ext):
     """Model G1 (unnormalized electric field autocorrelation) for Frequency Domain Diffuse correlation spectroscopy with Extrapolated Boundary Condition.
     Source: "Frequency Domain Diffuse Correlation Spectroscopy: A New Method for Simultaneous Estimation of Static and Dynamic Tissue Optical Properties"
     parameters:
-        tau := Correlation time [time]
-        bfi := Blood-Flow Index [length^2/time]
+        rho := Source-Detector Seperation [length]
         mua := Absorption Coefficent [1/length]
         musp := Reduced Scattering Coefficent [1/length]
         wavelength := Wavelength of Light [length]
-        rho := Source-Detector Seperation [length]
+        bfi := Blood-Flow Index [length^2/time]
+        tau := Correlation time [time]
+        spatial_freq := Spatial Frequncy of the Source in vacuum (`ξ = freq / c`) [1/length]
         n_media := Media Index of Refraction []
         n_ext := External Index of Refraction []
-        freq := Frequncy of Source [1/time]
-        c := Speed of Light in vacuum [length/time]
     """
     D = 1 / (3 * (mua + musp))
-    v = c / n_media
-    omega = 2 * pi * freq
+    src_k = 2 * pi * n_media * spatial_freq
     k0 = 2 * pi * n_media / wavelength
-    k = sqrt((mua + 2 * musp * k0**2 * bfi * tau - 1j * (omega / v)) / D)
+    k = sqrt((mua + 2 * musp * k0**2 * bfi * tau - 1j * src_k) / D)
     return abs(ecbc_reflectance(rho, k, mua, musp, n_media, n_ext))
 
 
-def model_g1_norm(tau, bfi, mua, musp, wavelength, rho, n_media, n_ext):
+def model_g1_norm(rho, mua, musp, wavelength, bfi, tau, n_media, n_ext):
     """Model g1 (normalized electric field autocorrelation) for Diffuse correlation spectroscopy with Extrapolated Boundary Condition.
     Source: "Diffuse optics for tissue monitoring and tomography"
     parameters:
-        tau := Correlation time [time]
-        bfi := Blood-Flow Index [length^2/time]
+        rho := Source-Detector Seperation [length]
         mua := Absorption Coefficent [1/length]
         musp := Reduced Scattering Coefficent [1/length]
         wavelength := Wavelength of Light [length]
-        rho := Source-Detector Seperation [length]
+        bfi := Blood-Flow Index [length^2/time]
+        tau := Correlation time [time]
         n_media := Media Index of Refraction []
         n_ext := External Index of Refraction []
     """
@@ -232,44 +228,42 @@ def g2_from_g1(g1, beta):
     return 1 + beta * abs_square(g1)
 
 
-def model_g2(tau, bfi, beta, mua, musp, wavelength, rho, n_media, n_ext):
+def model_g2(rho, mua, musp, wavelength, bfi, tau, beta, n_media, n_ext):
     """Model g2 (normalized intensity autocorrelation) for Diffuse correlation spectroscopy with Extrapolated Boundary Condition.
     Source: "Diffuse optics for tissue monitoring and tomography"
     parameters:
-        tau := Correlation time [time]
-        bfi := Blood-Flow Index [length^2/time]
-        beta := Beta derived for Siegert relation []
+        rho := Source-Detector Seperation [length]
         mua := Absorption Coefficent [1/length]
         musp := Reduced Scattering Coefficent [1/length]
         wavelength := Wavelength of Light [length]
-        rho := Source-Detector Seperation [length]
+        bfi := Blood-Flow Index [length^2/time]
+        tau := Correlation time [time]
+        beta := Beta derived for Siegert relation []
         n_media := Media Index of Refraction []
         n_ext := External Index of Refraction []
     """
-    g1 = model_g1_norm(tau, bfi, mua, musp, wavelength, rho, n_media, n_ext)
+    g1 = model_g1_norm(rho, mua, musp, wavelength, bfi, tau, n_media, n_ext)
     return g2_from_g1(g1, beta)
 
 
-def model_fd_g1_norm(tau, bfi, mua, musp, wavelength, rho, n_media, n_ext, freq, c):
+def model_fd_g1_norm(rho, mua, musp, wavelength, bfi, tau, spatial_freq, n_media, n_ext):
     """Model g1 (normalized electric field autocorrelation) for Frequency Domain Diffuse correlation spectroscopy with Extrapolated Boundary Condition.
     Source: "Frequency Domain Diffuse Correlation Spectroscopy: A New Method for Simultaneous Estimation of Static and Dynamic Tissue Optical Properties"
     parameters:
-        tau := Correlation time [time]
-        bfi := Blood-Flow Index [length^2/time]
+        rho := Source-Detector Seperation [length]
         mua := Absorption Coefficent [1/length]
         musp := Reduced Scattering Coefficent [1/length]
         wavelength := Wavelength of Light [length]
-        rho := Source-Detector Seperation [length]
+        bfi := Blood-Flow Index [length^2/time]
+        tau := Correlation time [time]
+        spatial_freq := Spatial Frequncy of the Source in vacuum (`ξ = freq / c`) [1/length]
         n_media := Media Index of Refraction []
         n_ext := External Index of Refraction []
-        freq := Frequncy of Source [1/time]
-        c := Speed of Light in vacuum [length/time]
     """
     D = 1 / (3 * (mua + musp))
-    v = c / n_media
-    omega = 2 * pi * freq
+    src_k = 2 * pi * n_media * spatial_freq
     k0 = 2 * pi * n_media / wavelength
-    k_tau = sqrt((mua + 2 * musp * k0**2 * bfi * tau - 1j * (omega / v)) / D)
+    k_tau = sqrt((mua + 2 * musp * k0**2 * bfi * tau - 1j * src_k) / D)
     k_norm = sqrt(mua / D)
     G1_norm = ecbc_reflectance(rho, k_norm, mua, musp, n_media, n_ext)
     return abs(ecbc_reflectance(rho, k_tau, mua, musp, n_media, n_ext)) / G1_norm
@@ -300,41 +294,38 @@ def expanded_fd_g2_from_g1(g1_dc, g1_ac, beta, mod_depth):
 
 
 def model_fd_g2_simplified(
-    tau,
-    bfi,
-    beta,
-    mod_depth,
+    rho,
     mua,
     musp,
     wavelength,
-    rho,
+    bfi,
+    tau,
+    spatial_freq,
+    beta,
+    mod_depth,
     n_media,
     n_ext,
-    freq,
-    c,
 ):
     """Model g2 (normalized intensity autocorrelation) for Frequency Domain Diffuse correlation spectroscopy with Extrapolated Boundary Condition.
     Source: "Frequency Domain Diffuse Correlation Spectroscopy: A New Method for Simultaneous Estimation of Static and Dynamic Tissue Optical Properties"
     parameters:
-        tau := Correlation time [time]
-        bfi := Blood-Flow Index [length^2/time]
-        beta := Beta derived for Siegert relation []
-        mod_depth := Source Modulation Depth []
+        rho := Source-Detector Seperation [length]
         mua := Absorption Coefficent [1/length]
         musp := Reduced Scattering Coefficent [1/length]
         wavelength := Wavelength of Light [length]
-        rho := Source-Detector Seperation [length]
+        bfi := Blood-Flow Index [length^2/time]
+        tau := Correlation time [time]
+        spatial_freq := Spatial Frequncy of the Source in vacuum (`ξ = freq / c`) [1/length]
+        beta := Beta derived for Siegert relation []
+        mod_depth := Source Modulation Depth []
         n_media := Media Index of Refraction []
         n_ext := External Index of Refraction []
-        freq := Frequncy of Source [1/time]
-        c := Speed of Light in vacuum [length/time]
     """
     # return 1 + beta * (1 - mod_depth) * model_g1(...)**2 + beta * mod_depth * abs_square(model_fd_g1(...))
     D = 1 / (3 * (mua + musp))
-    v = c / n_media
-    omega = 2 * pi * freq
+    src_k = 2 * pi * n_media * spatial_freq
     k0 = 2 * pi * n_media / wavelength
-    k_ac = sqrt((mua + 2 * musp * k0**2 * bfi * tau - 1j * (omega / v)) / D)
+    k_ac = sqrt((mua + 2 * musp * k0**2 * bfi * tau - 1j * src_k) / D)
     k_dc = sqrt((mua + 2 * musp * k0**2 * bfi * tau) / D)
     k_norm = sqrt(mua / D)
     G1_norm = ecbc_reflectance(rho, k_norm, mua, musp, n_media, n_ext)
@@ -344,41 +335,38 @@ def model_fd_g2_simplified(
 
 
 def model_fd_g2(
-    tau,
-    bfi,
-    beta,
-    mod_depth,
+    rho,
     mua,
     musp,
     wavelength,
-    rho,
+    bfi,
+    tau,
+    spatial_freq,
+    beta,
+    mod_depth,
     n_media,
     n_ext,
-    freq,
-    c,
 ):
     """Model g2 (normalized intensity autocorrelation) for Frequency Domain Diffuse correlation spectroscopy with Extrapolated Boundary Condition.
     Source: "Frequency Domain Diffuse Optics Spectroscopies for Quantitative Measurement of Tissue Optical Properties"
     parameters:
-        tau := Correlation time [time]
-        bfi := Blood-Flow Index [length^2/time]
-        beta := Beta derived for Siegert relation []
-        mod_depth := Source Modulation Depth []
+        rho := Source-Detector Seperation [length]
         mua := Absorption Coefficent [1/length]
         musp := Reduced Scattering Coefficent [1/length]
         wavelength := Wavelength of Light [length]
-        rho := Source-Detector Seperation [length]
+        bfi := Blood-Flow Index [length^2/time]
+        tau := Correlation time [time]
+        spatial_freq := Spatial Frequncy of the Source in vacuum (`ξ = freq / c`) [1/length]
+        beta := Beta derived for Siegert relation []
+        mod_depth := Source Modulation Depth []
         n_media := Media Index of Refraction []
         n_ext := External Index of Refraction []
-        freq := Frequncy of Source [1/time]
-        c := Speed of Light in vacuum [length/time]
     """
     # return 1 + beta * ((model_g1(...) + mod_depth * abs(model_fd_g1(...))) / (1 + mod_depth))**2
     D = 1 / (3 * (mua + musp))
-    v = c / n_media
-    omega = 2 * pi * freq
+    src_k = 2 * pi * n_media * spatial_freq
     k0 = 2 * pi * n_media / wavelength
-    k_ac = sqrt((mua + 2 * musp * k0**2 * bfi * tau - 1j * (omega / v)) / D)
+    k_ac = sqrt((mua + 2 * musp * k0**2 * bfi * tau - 1j * src_k) / D)
     k_dc = sqrt((mua + 2 * musp * k0**2 * bfi * tau) / D)
     k_norm = sqrt(mua / D)
     G1_norm = ecbc_reflectance(rho, k_norm, mua, musp, n_media, n_ext)
