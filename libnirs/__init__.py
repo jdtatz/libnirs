f9d80338a72191a775548c8cbe468336927fb58a
Julia Tatz <julia.tatz@childrens.harvard.edu>
2025-05-27 21:49:30 -0400

    deprecate top-level re-exports
    This will allow marking the numba models as deprecated and making numba itself optional

--- libnirs/__init__.py
+++ libnirs/__init__.py
@@ -1,14 +1,53 @@
-from .extinction_coeffs import deoxyhemoglobin, get_extinction_coeffs, oxyhemoglobin, soy_oil, water
-from .layered_model import (
-    model_nlayer_fd,
-    model_nlayer_fd_fft,
-    model_nlayer_g1,
-    model_nlayer_g1_fft,
-    model_nlayer_g2,
-    model_nlayer_ss,
-    model_nlayer_ss_fft,
-)
-from .model import model_fd, model_g1, model_g2, model_ss, model_td
-from .plotting import joint_hist, soft_dark_style
-from .statistical import CentralMoments, StandardMoments, WeightedCentralMoments, weighted_quantile
-from .utils import fma, gen_coeffs, gen_impedance, integrate, qrng
+_ext_coeffs = ["deoxyhemoglobin", "get_extinction_coeffs", "oxyhemoglobin", "soy_oil", "water"]
+_nlayer = [
+    "model_nlayer_fd",
+    "model_nlayer_fd_fft",
+    "model_nlayer_g1",
+    "model_nlayer_g1_fft",
+    "model_nlayer_g2",
+    "model_nlayer_ss",
+    "model_nlayer_ss_fft",
+]
+_model = ["model_fd", "model_g1", "model_g2", "model_ss", "model_td"]
+_plotting = ["joint_hist", "soft_dark_style"]
+_stat = ["CentralMoments", "StandardMoments", "WeightedCentralMoments", "weighted_quantile"]
+_util = ["fma", "gen_coeffs", "gen_impedance", "integrate", "qrng"]
+
+__all__ = _ext_coeffs + _nlayer + _model + _plotting + _stat + _util
+
+
+def __getattr__(name):
+    from warnings import warn
+
+    if name in _ext_coeffs:
+        import libnirs.extinction_coeffs
+
+        submod = libnirs.extinction_coeffs
+    elif name in _nlayer:
+        import libnirs.layered_model
+
+        submod = libnirs.layered_model
+    elif name in _model:
+        import libnirs.model
+
+        submod = libnirs.model
+    elif name in _plotting:
+        import libnirs.plotting
+
+        submod = libnirs.plotting
+    elif name in _stat:
+        import libnirs.statistical
+
+        submod = libnirs.statistical
+    elif name in _util:
+        import libnirs.utils
+
+        submod = libnirs.utils
+    else:
+        raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+
+    warn(
+        f"Importing {name} directly from libnirs.__init__ is deprecated. Use `from {submod.__name__} import {name}` instead",
+        FutureWarning,
+    )
+    return getattr(submod, name)
